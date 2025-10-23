f3a0b6be43aafce12a18f57a9babc6479bccf61e
Angelo Yana <angeloyana.pub@gmail.com>
2025-10-23 12:38:38 +0800

    feat: improve sidebar

--- client/src/routes/(app)/_components/app-sidebar.jsx
+++ client/src/routes/(app)/_components/app-sidebar.jsx
@@ -1,10 +1,14 @@
+import { ChevronDown, LogOut } from 'lucide-react';
 import { Link } from 'react-router';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
@@ -15,6 +19,20 @@ import { routes } from '../_lib/routes';
 export function AppSidebar() {
   return (
     <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg">
+              <Avatar>
+                <AvatarImage src="https://api.dicebear.com/9.x/pixel-art/svg?backgroundColor=b6e3f4,c0aede,d1d4f9" />
+                <AvatarFallback>KP</AvatarFallback>
+              </Avatar>
+              Account
+              <ChevronDown className="ml-auto" />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
@@ -33,6 +51,16 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <LogOut />
+              Log Out
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
