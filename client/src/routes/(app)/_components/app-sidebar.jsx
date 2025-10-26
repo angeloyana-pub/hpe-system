dcbd22ba084d21f5c4bee10eda6ffa2b4f951a88
Angelo Yana <angelocyana@gmail.com>
2025-10-26 08:08:45 +0800

    feat: improve sidebar ui

--- client/src/routes/(app)/_components/app-sidebar.jsx
+++ client/src/routes/(app)/_components/app-sidebar.jsx
@@ -1,9 +1,17 @@
-import { ChevronDown, LogOut } from 'lucide-react';
+import { LogOut } from 'lucide-react';
 import { Link } from 'react-router';
 import { toast } from 'sonner';
 
 import { useAuth } from '@/auth/context';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import {
   Sidebar,
   SidebarContent,
@@ -14,12 +22,14 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from '@/components/ui/sidebar';
 
 import { routes } from '../_lib/routes';
 
 export function AppSidebar() {
-  const { logout } = useAuth();
+  const { isMobile } = useSidebar();
+  const { user, logout } = useAuth();
 
   const handleLogout = async () => {
     await logout();
@@ -33,11 +43,15 @@ export function AppSidebar() {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg">
               <Avatar>
-                <AvatarImage src="https://api.dicebear.com/9.x/pixel-art/svg?backgroundColor=b6e3f4,c0aede,d1d4f9" />
-                <AvatarFallback>KP</AvatarFallback>
+                <AvatarImage src="/logo.png" />
+                <AvatarFallback>HP</AvatarFallback>
               </Avatar>
-              Account
-              <ChevronDown className="ml-auto" />
+              <div className="flex flex-col gap-0.5 leading-none">
+                <span className="truncate font-medium">Hydro-Pro</span>
+                <span className="truncate text-xs text-muted-foreground">
+                  Inventory and POS System
+                </span>
+              </div>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -63,10 +77,47 @@ export function AppSidebar() {
       <SidebarFooter>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton onClick={handleLogout}>
-              <LogOut />
-              Log Out
-            </SidebarMenuButton>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                  <Avatar>
+                    <AvatarImage src="https://api.dicebear.com/9.x/pixel-art/svg?backgroundColor=b6e3f4,c0aede,d1d4f9" />
+                    <AvatarFallback>KP</AvatarFallback>
+                  </Avatar>
+                  <div className="flex flex-col gap-0.5 leading-none">
+                    <span className="truncate font-medium">{user?.username}</span>
+                    <span className="truncate text-xs text-muted-foreground">Admin</span>
+                  </div>
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                side={isMobile ? 'bottom' : 'right'}
+                align="end"
+                sideOffset={4}
+              >
+                <DropdownMenuLabel>
+                  <div className="flex gap-2 items-center font-normal">
+                    <Avatar>
+                      <AvatarImage src="https://api.dicebear.com/9.x/pixel-art/svg?backgroundColor=b6e3f4,c0aede,d1d4f9" />
+                      <AvatarFallback>KP</AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col gap-0.5 leading-none">
+                      <span className="truncate font-medium">{user?.username}</span>
+                      <span className="truncate text-xs text-muted-foreground">Admin</span>
+                    </div>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleLogout}>
+                  <LogOut />
+                  Log Out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
